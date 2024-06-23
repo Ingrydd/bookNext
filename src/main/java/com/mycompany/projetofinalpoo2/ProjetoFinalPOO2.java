602be7c3b6f36de42611689d72624c7a6cd66408
Ingryd Belazzi <ingrydbelazzialves13@gmail.com>
2024-06-23 15:10:47 -0300

    adicionando as pastas MVC

--- src/main/java/com/mycompany/projetofinalpoo2/ProjetoFinalPOO2.java
+++ src/main/java/com/mycompany/projetofinalpoo2/ProjetoFinalPOO2.java
@@ -1,8 +1,19 @@
 package com.mycompany.projetofinalpoo2;
+import View.PrincipalGUI;
+
+/**
+Nomes: Bruna de Paula Oliveira
+       Ingryd Belazzi Alves
+       Samilly Emanuelle Lopes de Deus 
+ */
 
 public class ProjetoFinalPOO2 {
 
     public static void main(String[] args) {
-        
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                new PrincipalGUI().setVisible(true);
+            }
+        });        
     }
 }
